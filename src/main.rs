b53129f72a50d910668544ca3a423c5c6fa0c896
Robert Jomar Malate <robjmal@gmail.com>
2026-08-23 18:56:41 +0200

    feat: solved p1.16, tested against p1.4

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,39 @@ use nalgebra::Vector3;
 /// Test on example in Exercise 1.4
 fn problem_1_16() -> std::option::Option<String> {
     // Init values based on problem statement
-    let input_step_1 = Vector3::new(-1, -1, 1);
-    let target_step_1: i32 = -1; 
-    let w_step_1 = Vector3::new(0, 0, 0);
-    let y_hat_step_1 = w_step_1.dot(&input_step_1);
+    let input_targets: [(Vector3<i32>, i32); 8] = [  
+        // (x1, x2, xb), xb always = 1 since "always on"
+        (Vector3::new(-1, -1, 1), -1),
+        (Vector3::new(-1, 1, 1), -1),
+        (Vector3::new(1, -1, 1), 1),
+        (Vector3::new(1, 1, 1), 1),
+        (Vector3::new(-1, -1, 1), -1),
+        (Vector3::new(-1, 1, 1), -1),
+        (Vector3::new(1, -1, 1), 1),
+        (Vector3::new(1, 1, 1), 1),
+    ];
+    // Params of model
+    let mut weights = Vector3::new(0, 0, 0);
 
-    // Updated values
-    let mut weights = w_step_1.clone();
+    for (i, (input, target)) in input_targets.iter().enumerate() {
+        let y_hat = weights.dot(&input);
 
-    println!("target = {target_step_1}, y_hat = {y_hat_step_1}");
+        
 
-    // Case 1: target is positive, output is negative
-    if target_step_1 > 1 && y_hat_step_1 <= 0 {
-        weights = weights + input_step_1;
+        // Case 1: target is positive, output is negative
+        if *target > 0 && y_hat <= 0 {
+            weights = weights + input;
+        }
+        // Case 2: target is negative, output is positive 
+        else if *target < 0 && y_hat >= 0 {
+            weights = weights - input;
+        }
+        // Case 3 & 4: target and output are the same signs -> DO nothing
+        println!("Step {}", i+1);
+        println!("target = {target} |  y_hat = {y_hat}");
+        println!("Updated parameters | w1: {}, w2: {}, b: {}", weights[0], weights[1], weights[2]);
+        println!("");
     }
-    // Case 2: target is negative, output is positive 
-    else if target_step_1 < 0 && y_hat_step_1 >= 0 {
-        weights = weights - input_step_1;
-    }
-    // Case 3 & 4: target and output are the same signs -> DO nothing
-
-    println!("w = {weights}");
 
     Some("problem is correct".to_string())
 }
