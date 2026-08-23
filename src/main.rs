2ad0a8a5e48698f3e91a162b1f612760c5f12187
Robert Jomar Malate <robjmal@gmail.com>
2026-08-23 19:06:49 +0200

    refactor: making it able to take in different input_targets

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,14 @@ use nalgebra::Vector3;
 /// Problem 1.16: Implement Perceptron Learning Rule (PLR)
 /// 
 /// Test on example in Exercise 1.4
-fn problem_1_16() -> std::option::Option<String> {
-    // Init values based on problem statement
-    let input_targets: [(Vector3<i32>, i32); 8] = [  
-        // (x1, x2, xb), xb always = 1 since "always on"
-        (Vector3::new(-1, -1, 1), -1),
-        (Vector3::new(-1, 1, 1), -1),
-        (Vector3::new(1, -1, 1), 1),
-        (Vector3::new(1, 1, 1), 1),
-        (Vector3::new(-1, -1, 1), -1),
-        (Vector3::new(-1, 1, 1), -1),
-        (Vector3::new(1, -1, 1), 1),
-        (Vector3::new(1, 1, 1), 1),
-    ];
+fn problem_1_16(input_targets: &[(Vector3<i32>, i32); 8]) -> std::option::Option<String> {
     // Params of model
     let mut weights = Vector3::new(0, 0, 0);
 
     for (i, (input, target)) in input_targets.iter().enumerate() {
         let y_hat = weights.dot(&input);
 
-        
-
+        // --- Perceptron Learning Rule ----
         // Case 1: target is positive, output is negative
         if *target > 0 && y_hat <= 0 {
             weights = weights + input;
@@ -44,5 +31,16 @@ fn problem_1_16() -> std::option::Option<String> {
 
 fn main() {
     println!("---- Chapter 1 Problems ----");
-    problem_1_16();
+    let test_01_p1_4: [(Vector3<i32>, i32); 8] = [  
+        // (x1, x2, xb), xb always = 1 since "always on"
+        (Vector3::new(-1, -1, 1), -1),
+        (Vector3::new(-1, 1, 1), -1),
+        (Vector3::new(1, -1, 1), 1),
+        (Vector3::new(1, 1, 1), 1),
+        (Vector3::new(-1, -1, 1), -1),
+        (Vector3::new(-1, 1, 1), -1),
+        (Vector3::new(1, -1, 1), 1),
+        (Vector3::new(1, 1, 1), 1),
+    ];
+    problem_1_16(&test_01_p1_4);
 }
